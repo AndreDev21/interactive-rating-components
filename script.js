199a98ace713719d5bcbc5e57661cd236b717946
AndreDev21 <andredev2103@gmail.com>
2022-11-12 23:03:50 -0300

    added the Script and the thank-you.html page and it´s css,and changed a bit of the html for the script to work

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('1').addEventListener("click", function(){
+    window.localStorage.setItem('btn','1')
+});
+document.getElementById('2').addEventListener("click", function(){
+    localStorage.setItem('btn','2')
+});
+document.getElementById('3').addEventListener("click", function(){
+    localStorage.setItem('btn','3')
+});
+document.getElementById('4').addEventListener("click", function(){
+    localStorage.setItem('btn','4')
+});
+document.getElementById('5').addEventListener("click", function(){
+    localStorage.setItem('btn','5')
+});
+
+
+
+
+
