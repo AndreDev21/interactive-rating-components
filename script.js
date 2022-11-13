1531341e4b8dfdfe7f6e5b2586d61a82f1378ba0
AndreDev21 <andredev2103@gmail.com>
2022-11-13 00:43:15 -0300

    fineshed project and added a good README

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.getElementById('1').addEventListener("click", function(){
-    window.localStorage.setItem('btn','1')
+    window.sessionStorage.setItem('btn','1')
 });
 document.getElementById('2').addEventListener("click", function(){
-    localStorage.setItem('btn','2')
+    sessionStorage.setItem('btn','2')
 });
 document.getElementById('3').addEventListener("click", function(){
-    localStorage.setItem('btn','3')
+    sessionStorage.setItem('btn','3')
 });
 document.getElementById('4').addEventListener("click", function(){
-    localStorage.setItem('btn','4')
+    sessionStorage.setItem('btn','4')
 });
 document.getElementById('5').addEventListener("click", function(){
-    localStorage.setItem('btn','5')
+    sessionStorage.setItem('btn','5')
 });
 
 
